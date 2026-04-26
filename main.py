d02a59dfa6a4491bb3f5dba92d566bbac87df559
LeelandA <leelandacuna7@gmail.com>
2026-04-26 06:45:29 +0000

    UD3 Complete

--- main.py
+++ main.py
@@ -1,30 +1,78 @@
 '''
-DEVELOPER(S): <your name(s)>
-COLLABORATORS: <anyone who helped you>
-DATE: <date last worked on>
+DEVELOPER(S): Leeland Acuna
+COLLABORATOR(S): None
 '''
-
 """
-A one line summary of the program, terminated by a period.
+Wordle Recreation: I play wordle almost everyday and I thought it would be a good idea if I were to recreate it
 
-Leave one blank line.  The rest of this docstring should contain an
-overall description of the program.
+
 """
 
 ##########################################
 # IMPORTS:
-# <list of modules needed for the program and their purpose>
+#   <list modules needed for program and their purpose>
 ##########################################
-# <replace this line with import statement(s)>
-
+#<replace this line with import statement(s)>
+import random
 
 ##########################################
 # FUNCTIONS:
 ##########################################
-# <replace this line with function definitions, each needs a description>
+#This function takes the guess input by the user and the word and compares the word inputed by the user and the correct word and returns the result
+def guess_word(guess, word):
+    result = ""
+    for char in range(len(word)):        
+        if guess[char] == word[char]:
+            result += guess[char]
+        
+        elif guess[char] in word:
+            result += "?"
+        
+        else:
+            result += "_"
+    
+    return result
+
+#This function gets the words in files
+def get_words(filename):
+    words = [] #chose to use a list since its easier to randomize a choice of words
+
+    in_file = open(filename, 'r')
+    for line in in_file:
+        word = line.strip()
+        if word != "":
+            words.append(word.upper())
+    in_file.close()
+    return words
+
+
 
 
 ##########################################
 # MAIN PROGRAM:
 ##########################################
-# <replace this line with your main program>
+user_mode = ""
+
+while user_mode != "R" and user_mode != "C":
+    user_mode = input("Would you like to play regular wordle or play the challenge mode(R (Regular)/C (Challenge)): ").upper()
+    if user_mode == "R":
+        words = get_words("wordle.txt")
+    elif user_mode == "C":
+        words = get_words("challenge.txt")
+    else:
+        print("Invalid choice.")
+
+word = random.choice(words)
+word_letter_amount = len(word)
+guess = input(f"Guess the {word_letter_amount} letter word: ").upper()
+while guess != word:
+    if len(guess) == len(word):
+        result = guess_word(guess, word)
+        print(result)
+        guess = input(f"Guess the {word_letter_amount} letter word:").upper()
+    else:
+        print("Wrong amount of letters")
+        guess = input(f"Guess the {word_letter_amount} letter word:").upper()
+
+
+print("Congratulations! You got the word!")
